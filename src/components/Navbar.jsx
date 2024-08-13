f13d8e5a458f89df439e5f2b8dbb710368c446cb
omkushwaha9 <omkushwaha2005@gmail.com>
2024-08-13 22:38:05 +0530

    Navbar Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,12 @@ import logop from '../images/logop.png'
 function Navbar() {
   return (
 
-    <div className=' w-full px-16 py-5 font-["Neue Montreal"] flex justify-between items-center '>
+    <div className=' w-full px-16  font-["Neue Montreal"] flex justify-between items-center '>
       <div className='Title'>
-      <img className=' w-48'  src={logop} alt='Profile'/>
+      <img className=' w-48 py-5'  src={logop} alt='Profile'/>
         
       </div>
-      <div className='links flex gap-10'>
+      <div className='links flex gap-10 py-3'>
         {["About", "Our Products", "Our Services", "Research", "Contact Us"].map((item, index) => (
           <a key={index} className={`text-lg font-light ${index === 4 && "ml-32"}`}>{item}</a>
         ))}
